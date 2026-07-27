f44dfb0c71b435c6b298380ac9a7024b7033192b
Sunita Tamang <sunutmg123@gmail.com>
2026-07-26 23:26:14 -0400

    updtae

--- wedding_planning_website_starter.jsx
+++ wedding_planning_website_starter.jsx
@@ -25,7 +25,7 @@ const vendors = [
     location: "Jersey City, NJ",
     price: "$150–$700",
     rating: 4.8,
-    image: "public/henna.png",
+    image: "/henna.png",
   },
   {
     name: "Sona Jewelry Studio",
@@ -96,7 +96,7 @@ export default function WeddingPlanningWebsiteStarter() {
         <div className="relative">
           <div className="absolute -inset-4 bg-amber-200 rounded-[2rem] rotate-3" />
           <img
-            src="public/Ring.jpg"
+            src="/Ring.jpg"
             alt="Nepali bride and groom in traditional wedding attire"
             className="relative rounded-[2rem] shadow-2xl object-cover h-[520px] w-full"
           />
